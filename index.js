2441d70aa24cc305b4589b995383f1bdd38dd616
Md Eftequarul Alam <efthaqur@gmail.com>
2025-10-30 00:40:02 +0600

    Update: Data ftech server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const data = require('./data-cs.json');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,7 +9,16 @@ app.get('/', (request, response) => {
         padding: 10px; color: #437e3eff; '>Server Home! at Port:${port} </h1>`)
 });
 
+app.get('/data', (request, response)=>{
+    response.send(data);
+});
 
+app.get('/data/:id', (request, response)=>{
+   const id = parseInt(request.params.id);
+    console.log('This is ID: ', id);
+    const title = data.find(d => d.id ===id) || {Err: 'Not Found'};
+   response.send(title);
+});
 
 app.listen(port, (response)=> {
     console.log(`User server started on Port: ${port}`);
