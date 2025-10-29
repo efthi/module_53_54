4e7da0c8d9c4131fe4e3c5351ae15a30779e0f96
Md Eftequarul Alam <efthaqur@gmail.com>
2025-10-30 00:21:50 +0600

    Update: / page updated

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const express = require ('express');
 const app = express();
-serverpage = require('./data-cs.json');
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-    response.send('<h1>Server Home!</h1>')
-});
-app.get('/serve', (request, response) => {
-    response.send(serverpage)
+    response.send(`<h1 style='text-align:center; 
+        padding: 10px; color: #437e3eff; '>Server Home! at Port:${port} </h1>`)
 });
 
-app.get('/data', (request, response) => {
-    response.send('<h1>Data is loading! </h1>')
-});
-app.get('/check', (request, response) => {
-    response.send('<h1>Check server! </h1>')
-});
+
 
 app.listen(port, (response)=> {
     console.log(`User server started on Port: ${port}`);
